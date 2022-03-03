d573e16fa401398cfd5419aeef289960d25e9edf
FLORBELA FREITAS OLIVEIRA <21276327@CFP127ALUNO.LOCAL>
2022-03-03 15:48:27 -0300

    adicionando arquivos

---
+++ produtos/main.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const produtos = [
+    {
+        id: 1, 
+        nome: 'Microfone Kingston', 
+        descricao: "Microfone Kingston HyperX QuadCast USB", 
+        avaliacao: 5,  
+        preco: 999.99, 
+        parcelamento: 'ou 10x de 99.99 sem juros',
+        imagem: './img/microfone_kingston_hyperx_quadcast.png'
+    },
+    {
+        id: 2, 
+        nome: 'Monitor AOC', 
+        descricao: "Monitor Gamer Curvo 240Hz Full HD 27” AOC", 
+        avaliacao: 3,  
+        preco: 2500, 
+        parcelamento: 'ou 10x de 250 sem juros',
+        imagem: './img/monitor_gamer_curvo.png'
+    },
+    {
+        id: 3, 
+        nome: 'Mouse Logitech', 
+        descricao: "Mouse Sem Fio M170 Nano LOGITECH Prata", 
+        avaliacao: 2,  
+        preco: 350, 
+        parcelamento: 'ou 10x de 35 sem juros',
+        imagem: './img/mouse.png'
+    },
+]
